4745a84d29be0235c391deec2f3d663b27700983
rocha101 <eduardoprudenciorocha@gmail.com>
2025-07-03 14:33:04 -0300

    chore: Update commitlint configuration to enforce header length and disable subject case and full stop rules. Simplify commit message validation process. Update Husky commit-msg hook to streamline execution.

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,1 +1,8 @@
-module.exports = { extends: ["@commitlint/config-conventional"] };
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "header-max-length": [2, "always", 300],
+    "subject-case": [0],
+    "subject-full-stop": [0],
+  },
+};
